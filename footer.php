004985f7dd2f7d607e6b676824ca3ff80f64ae96
golnoosh72 <41442666+golnoosh72@users.noreply.github.com>
2018-07-27 09:32:49 -0400

    Add files via upload
    Front End

---
+++ footer.php
@@ -0,0 +1,10 @@
+<div class = row>
+<div class="col-sm-12" style="text-align: center">
+
+Acknowledgment: content and pictures of this website are copy righted by Subway and are for educational purpose only.</p>
+</div>
+</div>
+</div>
+</body>
+
+</html>
