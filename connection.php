fc4383fa3dae0181729f6d61667c52554f0b88ba
Nasrin Izadi <izadivahedi.nasrin@gmail.com>
2018-08-09 16:34:56 -0400

    Accessibility fixes

--- connection.php
+++ connection.php
@@ -1,25 +1,31 @@
 <?php
-class Db {
+
+class Db
+{
 
     private static $instance = NULL;
 
     public static function getInstance()
     {
-        if (!isset(self::$instance))
-        {
+        if (!isset(self::$instance)) {
             $pdo_options[PDO::ATTR_ERRMODE] = PDO::ERRMODE_EXCEPTION;
 
-            if (strpos($_SERVER['HTTP_HOST'],"localhost")!==false){ // For Test on Localhost
+            if (strpos($_SERVER['HTTP_HOST'], "localhost") !== false) { // For Test on Localhost
                 $DB_host = "localhost";
                 $DB_user = "root";
                 $DB_pass = "root";
                 $DB_name = "iEat";
-            }else{
+            } elseif (strpos($_SERVER['HTTP_HOST'], "rikaweb.com") !== false) { //  # FROM TEST ENVIRONMENT CALLS
+                $DB_host = "rikaweb.com";
+                $DB_user = "root";
+                $DB_pass = "root";
+                $DB_name = "iEat";
+            } else {
                 die("Error");
             }
 
             self::$instance = new PDO(
-                'mysql:host='.$DB_host.';dbname='.$DB_name.'', $DB_user, $DB_pass, $pdo_options);
+                'mysql:host=' . $DB_host . ';dbname=' . $DB_name . '', $DB_user, $DB_pass, $pdo_options);
         }
         return self::$instance;
     }
