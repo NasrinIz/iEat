4afcc935995231eda3cfa0ae6f3c6821ea625e2d
Nasrin Izadi <izadivahedi.nasrin@gmail.com>
2018-08-09 16:38:26 -0400

    Connection changed

--- connection.php
+++ connection.php
@@ -2,28 +2,16 @@
 
 class Db
 {
-
     private static $instance = NULL;
 
     public static function getInstance()
     {
         if (!isset(self::$instance)) {
             $pdo_options[PDO::ATTR_ERRMODE] = PDO::ERRMODE_EXCEPTION;
-
-            if (strpos($_SERVER['HTTP_HOST'], "localhost") !== false) { // For Test on Localhost
-                $DB_host = "localhost";
-                $DB_user = "root";
-                $DB_pass = "root";
-                $DB_name = "iEat";
-            } elseif (strpos($_SERVER['HTTP_HOST'], "rikaweb.com") !== false) { //  # FROM TEST ENVIRONMENT CALLS
-                $DB_host = "rikaweb.com";
-                $DB_user = "root";
-                $DB_pass = "root";
-                $DB_name = "iEat";
-            } else {
-                die("Error");
-            }
-
+            $DB_host = "localhost";
+            $DB_user = "root";
+            $DB_pass = "root";
+            $DB_name = "iEat";
             self::$instance = new PDO(
                 'mysql:host=' . $DB_host . ';dbname=' . $DB_name . '', $DB_user, $DB_pass, $pdo_options);
         }
